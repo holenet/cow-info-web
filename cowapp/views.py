447e36db75e8d06f4a2226b31b74c5d26fbba876
holenet <holenet6147@naver.com>
2018-04-21 03:31:20 +0900

    test for product pull3

--- cowapp/views.py
+++ cowapp/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 
 def test(request):
-    return HttpResponse('It is cowapp test page. Wow It\'s successful!! 2222222222')
+    return HttpResponse('It is cowapp test page. Wow It\'s successful!! 2222222222 3333333333333333')
