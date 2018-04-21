268533d4da358908eb5104e838299fc6483fbb5f
holenet <holenet6147@naver.com>
2018-04-21 13:08:06 +0900

    CowSerailizer: set cow number restriction

--- cowapp/serializers.py
+++ cowapp/serializers.py
@@ -3,7 +3,19 @@ from rest_framework import serializers
 from cowapp.models import Cow
 
 
+def cow_number_restriction(num):
+    if len(num)==15:
+        arr = num.split('-')
+        if len(arr)==4:
+            if len(arr[0])==3 and len(arr[1])==len(arr[2])==4 and len(arr[3])==1:
+                if all([y.isdigit() for x in arr for y in x]):
+                    return num
+    raise serializers.ValidationError("pattern is not valid")
+
+
 class CowSerializer(serializers.ModelSerializer):
+    number = serializers.CharField(validators=[cow_number_restriction])
+
     class Meta:
         model = Cow
         fields = ('id', 'created', 'number', 'sex', 'birthday', 'mother')
