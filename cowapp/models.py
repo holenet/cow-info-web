c4f6255b1f2c53dc288634a134388dfa587ee7d8
holenet <holenet6147@naver.com>
2018-06-22 18:45:10 +0900

    modify models

--- cowapp/models.py
+++ cowapp/models.py
@@ -3,14 +3,14 @@ from django.db import models
 
 class Cow(models.Model):
     created = models.DateTimeField(auto_now_add=True)
-    number = models.TextField()
+    number = models.TextField(unique=True)
     sex = models.TextField()
-    birthday = models.DateTimeField(null=True, blank=True)
-    mother = models.ForeignKey('cowapp.Cow', related_name='children', on_delete=models.DO_NOTHING, null=True, blank=True)
+    birthday = models.DateField(null=True, blank=True)
+    mother = models.ForeignKey('cowapp.Cow', related_name='children', on_delete=models.SET_NULL, null=True, blank=True)
     user = models.ForeignKey('auth.User', related_name='cows', on_delete=models.CASCADE)
 
     class Meta:
-        ordering = ('created',)
+        ordering = ('birthday', 'created',)
 
     def __str__(self):
         return self.number
@@ -21,11 +21,11 @@ class Record(models.Model):
     cow = models.ForeignKey('cowapp.Cow', related_name='records', on_delete=models.CASCADE)
     content = models.TextField()
     etc = models.TextField(null=True, blank=True)
-    day = models.DateTimeField()
+    day = models.DateField()
     user = models.ForeignKey('auth.User', related_name='records', on_delete=models.CASCADE)
 
     class Meta:
-        ordering = ('created',)
+        ordering = ('day', 'created',)
 
     def __str__(self):
         return "{}: {}".format(self.cow.number, self.content)
