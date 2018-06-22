1457348eeeac370937f344c9796211572a9855c9
holenet <holenet6147@naver.com>
2018-06-22 18:47:33 +0900

    space fix

--- cowapp/urls.py
+++ cowapp/urls.py
@@ -7,6 +7,7 @@ app_name = 'cowapp'
 urlpatterns = [
     path('cows/', views.CowList.as_view()),
     path('cows/<int:pk>/', views.CowDetail.as_view()),
+
     path('records/', views.RecordList.as_view()),
     path('records/<int:pk>/', views.RecordDetail.as_view()),
     path('records/cow/<int:pk>/', views.RecordList.as_view()),
